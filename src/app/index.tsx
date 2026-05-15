40499f5cb0b3e464c8755edea0b77672c9c21a5e
Aurélien Grimaud <a.grimaud@involt.io>
2026-05-15 10:56:40 +0200

    feat: migrate from React to Preact and update ESLint and TypeScript configurations

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import React from 'react'
 import mixpanel from 'mixpanel-figma'
 import { TolgeeProvider } from '@tolgee/react'
 import * as Sentry from '@sentry/react'
